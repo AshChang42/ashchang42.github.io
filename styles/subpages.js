569d1c4450aaaa9d429cc0558cc96bc7e59cb9b7
張雨平 <ashchang42@gmail.com>
2021-08-19 21:14:33 +0800

    0819_補傳

---
+++ styles/subpages.js
@@ -0,0 +1,9 @@
+const targetDiv = document.getElementById("magister");
+const btn = document.getElementById("mag_toggle");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+};
